920dcee8acc1c04a4c28819952c3031e7a919fa7
Upasana Purohit <67505447+upasana77@users.noreply.github.com>
2022-11-28 04:45:38 -0500

    Update firstpythonex.py

--- firstpythonex.py
+++ firstpythonex.py
@@ -1,2 +1,4 @@
 # Display the output
+# Here we learn how to create a new subfolder in the repo, using the "create new file" option,step1) provide a name to subfolder
+# step 2: provide description, step3. commit changes
 print("New Python file")
