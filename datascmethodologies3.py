8fcb3fa86d8b1d590340c3e95cd6329c908fcd40
Upasana Purohit <67505447+upasana77@users.noreply.github.com>
2022-11-28 11:47:14 -0500

    Update datascmethodologies3.py

--- datascmethodologies3.py
+++ datascmethodologies3.py
@@ -1,1 +1,15 @@
 ##Data Science Methodologises:
+# The data Science Methodology aim to answer the following 10 questions in this prescribed sequence.
+# FROM PROBLEM TO APPROACH:
+# 1.What is the problem that you are trying to solve?
+# 2.How can you use data to ansewer the questions?
+# WORKING WITH THE DATA:
+#3.What data do you need to answer the questions?
+#4.Where is the data coming from (identify all sources) and how will you get it?
+#5.Is the data that you collected representative of the problem to be solved?
+#6. What additional work is required to manupulate and work with the data.
+#DERIVING THE ANSWER
+#7.In what way can the data be visualized to get to the answer that is required?
+#8.Does the model used really answer the initial question or does it need to be adjusted?
+#9.Can you put the model into practice?
+#10.Can you get constructive feedback into answering the questions?
