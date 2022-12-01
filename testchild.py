70b7435c184d0e3e502a4b09c5566bd960ad9422
Upasana Purohit <67505447+upasana77@users.noreply.github.com>
2022-12-01 16:09:32 -0500

    Delete testchild.py

--- testchild.py
+++
@@ -1,6 +0,0 @@
-## Adding a new file in child branch
-# first I created a child_branch.
-# Write my required information in the child-branch.
-# and check the changes by clicking on top right corner, pull and merge request.
-#and finally merge with the main file
-print("Inside Child branch")
